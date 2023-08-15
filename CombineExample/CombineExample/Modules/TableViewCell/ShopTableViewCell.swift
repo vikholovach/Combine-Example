fa31c7da53658b96d4975ae7a56f58bf861dbbe4
Viktor Holovach <viktorgolovach@Viktors-MacBook-Pro.local>
2023-08-15 17:58:39 +0200

    add MVP base structure and Network Service layer

---
+++ CombineExample/CombineExample/Modules/TableViewCell/ShopTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShopTableViewCell.swift
+//  CombineExample
+//
+//  Created by Viktor Golovach on 15.08.2023.
+//
+
+import UIKit
+
+class ShopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CombineExample/CombineExample/PresentationLayer/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  CombineExample
+//
+//  Created by Viktor Golovach on 15.08.2023.
+//
+
+import Foundation
